5c7b9a4fa6a9eac954865ffbfb903bc5649e44b1
lazyninjacat <matthew.riddett@gmail.com>
2020-03-18 19:14:35 -0700

    march 18 catch up

--- Assets/Scripts/VW_Gameloop.cs
+++ Assets/Scripts/VW_Gameloop.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.IO;
 using System;
